85f67e36ea0c3c2f89dd3ff5479435f4d5e7f7fc
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-06-04 01:52:54 +0600

    trying to apply JS on images

---
+++ ImageVault/src/imgShowing.js
@@ -0,0 +1,5 @@
+const img = document.querySelector("#im11");
+// const showDetails = document.querySelector("#show-details");
+const showTable = document.querySelector("#show-table");
+console.log(showTable);
+// console.log(showDetails);
